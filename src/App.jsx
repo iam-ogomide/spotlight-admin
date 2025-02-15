6980e741a513d384ff37b1cdbab8047a61f537e3
iam-ogomide <ogomidesamuel10@gmail.com>
2025-02-15 03:58:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { ThemeProvider } from "@/contexts/theme-context"
+import Layout from "./routes/Layout"
+import DashboardPage from "./routes/dashboard/page"
+import Analytics from "./routes/dashboard/Analytics"
+
+
+
+
+function App() {
+  const router = createBrowserRouter([
+    // {
+    //   path:"/anl",
+    //   element: <Analytics />
+    // },
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <DashboardPage />
+        },
+        {
+          path: "analytics",
+          element: <Analytics />,
+      },
+      {
+          path: "reports",
+          element: <h1 className="title">Reports</h1>,
+      },
+      {
+          path: "customers",
+          element: <h1 className="title">Customers</h1>,
+      },
+      {
+          path: "new-customer",
+          element: <h1 className="title">New Customer</h1>,
+      },
+      {
+          path: "verified-customers",
+          element: <h1 className="title">Verified Customers</h1>,
+      },
+      {
+          path: "products",
+          element: <h1 className="title">Products</h1>,
+      },
+      {
+          path: "new-product",
+          element: <h1 className="title">New Product</h1>,
+      },
+      {
+          path: "inventory",
+          element: <h1 className="title">Inventory</h1>,
+      },
+      {
+          path: "settings",
+          element: <h1 className="title">Settings</h1>,
+      },
+      ]
+    }
+  ])
+
+  return (
+    <ThemeProvider storageKey="theme">
+       <RouterProvider router={router} />
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/routes/dashboard/Analytics.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Analytics = () => {
+  return (
+    <div>Analytics is probabaly something crazy</div>
+  )
+}
+
+export default Analytics
